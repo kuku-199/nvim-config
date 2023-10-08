48f8640614d607c9910869afe3129ae27d674834
Thorn <2981263629@qq.com>
2023-10-08 15:42:19 +0800

    Updata for keymaps

--- lua/lib/keymaps.lua
+++ lua/lib/keymaps.lua
@@ -5,6 +5,10 @@ local keymap = vim.keymap
 -- mode
 keymap.set("i",",.","<ESC>") --|mode|,|now key|,|old key|
 
+-- move 
+keymap.set("v","J",":m '>+1<CR>gv=gv")
+keymap.set("v","K",":m '<-2<CR>gv=gv")
 
-
-
+-- window
+keymap.set("n","<leader>sv","<C-w>v") -- vertical new window
+keymap.set("n","<leader>sh","<C-w>s") -- horizontal new window
