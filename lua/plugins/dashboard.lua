579ed050e2be214f7665cdbe080dec3ea5243699
Emotion-Thorn <emotion.thorn@gmail.com>
2023-12-04 11:40:40 +0800

    Add dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,11 @@
+return 
+{
+  'glepnir/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
