0c9b1bd4b37b1083e86491ac9a5e73d2d8818002
Emotion-Thorn <emotion.thorn@gmail.com>
2023-12-05 11:40:46 +0800

    Updata nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,29 +1,12 @@
 return
 {
-   'nvim-tree/nvim-tree.lua',
-   "nvim-tree/nvim-web-devicons",
-   config = function()
-     --confing
- -- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
+ "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
   },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+  config = function()
+    require("nvim-tree").setup {}
   end,
 }
